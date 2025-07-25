ccc6087b680df11ab482e1046e901ad5e15cb7cb
Tamim Islam <nottamimislam@gmail.com>
2025-07-25 08:34:37 +0600

    Deploy

--- petnest/settings.py
+++ petnest/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app', '127.0.0.1']  # Allow all hosts for local development
+ALLOWED_HOSTS = ['petnest-backend.vercel.app', '127.0.0.1']  # Allow all hosts for local development
 
 # Application definition
 INSTALLED_APPS = [
